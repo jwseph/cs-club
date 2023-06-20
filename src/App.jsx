5dc0fe96cf7560de81b9220c70d3fd9d0914caf0
jwseph <kodomo.oishii@gmail.com>
2023-06-19 21:51:21 -0700

    Member cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
 import { useState } from 'react'
 import './App.css'
 
+import { HiOutlineExternalLink } from 'react-icons/hi';
 import { HiChevronDown } from 'react-icons/hi2';
 import { BsInstagram, BsYoutube } from 'react-icons/bs';
+
+function CardTag({href, children}) {
+  return (
+    <a
+      className='flex items-center gap-1 px-4 py-2 border border-violet-500 text-violet-500 rounded-full hover:bg-violet-500/10 duration-500 ease-in-out'
+      href={href} target='_blank'
+    >
+      <span className='text-xs font-semibold'>{children}</span>
+      <HiOutlineExternalLink className='w-4 h-4'/>
+    </a>
+  )
+}
+
+function Card({name, quote, image, children}) {
+  return (
+    <div className='flex flex-col items-center w-96 px-8 py-8 space-y-6 border border-white rounded-lg hover:shadow-2xl hover:shadow-violet-300/10 hover:scale-105 duration-500 ease-in-out'>
+      <img src={image} className='aspect-square w-full'/>
+      <div className='flex flex-col items-center space-y-1'>
+        <h3 className='uppercase text-2xl font-bold tracking-tight text-center'>{name}</h3>
+        <p className='text-base italic text-center'>{quote}</p>
+      </div>
+      <div className='flex flex-wrap w-full gap-4'>
+        {children}
+      </div>
+    </div>
+  )
+}
 
 function App() {
   return (
@@ -29,8 +57,17 @@ function App() {
           </div>
         </div>
       </div>
-      <div className='flex flex-col min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8 bg-slate-950 text-base select-none'>
-        <h1 className='text-2xl font-semibold tracking-tight'>Coming soon!</h1>
+      <div className='flex flex-col justify-center py-64 px-8 lg:px-20 bg-slate-950 text-base select-none'>
+        <h1 className='font-extrabold tracking-tighter text-6xl'>The members</h1>
+        <div className='flex flex-wrap pt-8 gap-8'>
+          <Card name='Dylan Kim' quote='I dream of computer science' image='/dylan.jpg'>
+          </Card>
+          <Card name='Joseph Jackson' quote='Guys, add more padding' image='/joseph.jpg'>
+            <CardTag href='https://kamiak.org/sniperbot'>Sniper Bot</CardTag>
+            <CardTag href='https://yuu.pages.dev'>Yuu Player</CardTag>
+          </Card>
+        </div>
+        <h1 className='pt-64 text-2xl font-semibold tracking-tight text-center'>More coming soon!</h1>
       </div>
       <div className='bg-slate-950'>
         <div
